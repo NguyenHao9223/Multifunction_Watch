067d94533c28df65ece3cb6d29353f2f8b36e2bd
NguyenHao9223 <nguyenvanhao5970@gmail.com>
2023-03-28 12:40:19 +0700

    Second

--- TEST_DS1307_DHT11_OLED/TEST_DS1307_DHT11_OLED.ino
+++ TEST_DS1307_DHT11_OLED/TEST_DS1307_DHT11_OLED.ino
@@ -20,12 +20,14 @@ const float NHIET_DO_CAO = 35.0; //Khai bao 1 bien nhiet do cao = 35 do C
 byte Buzzer = 7;
 
 byte _day, _month, _hour24, _hour12, _minute, _second, _dtw;
-int _year;
+int _year;  //Biến _year được sử dụng để lưu trữ giá trị của năm, và giá trị năm có thể lớn hơn 255 (giá trị tối đa mà một biến byte có thể lưu trữ).
+            // Vì vậy, để đảm bảo rằng biến có thể lưu trữ tất cả các giá trị năm, biến _year được định nghĩa là kiểu int, có thể lưu trữ giá trị từ -32768 đến 32767.
 byte hr24;
 char st[3];
 char _nameoftheDay[9];
 char _monthsName[3];
 
+//định nghĩa một mảng các chuỗi ký tự (hay còn gọi là mảng các strings) để lưu trữ tên của các ngày trong tuần.
 const char daysOfTheWeek_0[] PROGMEM = "Sunday";   
 const char daysOfTheWeek_1[] PROGMEM = "Monday";
 const char daysOfTheWeek_2[] PROGMEM = "Tuesday";
@@ -35,6 +37,8 @@ const char daysOfTheWeek_5[] PROGMEM = "Friday";
 const char daysOfTheWeek_6[] PROGMEM = "Saturday";
 
 const char* const daysOfTheWeek_table[] PROGMEM = {daysOfTheWeek_0, daysOfTheWeek_1, daysOfTheWeek_2, daysOfTheWeek_3, daysOfTheWeek_4, daysOfTheWeek_5, daysOfTheWeek_6};
+//const char* const, có chứa các con trỏ (pointers) tới các chuỗi ký tự được lưu trữ trong các mảng daysOfTheWeek_X.
+//Mảng này cũng được đánh dấu là PROGMEM để lưu trữ trong bộ nhớ flash của vi điều khiển, giúp giảm tải cho bộ nhớ RAM của vi điều khiển.
 
 const char months_name_0[] PROGMEM = "Jan";   
 const char months_name_1[] PROGMEM = "Feb";
@@ -78,26 +82,7 @@ void loop() {
   GetDateTime(); 
   display.clearDisplay(); 
   Digital_Clock_and_Date(_hour12, _minute, _second, _dtw, _day, _month ,_year, st); 
-
-  float temperature = dht.readTemperature();  // Đọc giá trị nhiệt độ từ DHT11
-  float humidity = dht.readHumidity();  // Đọc giá trị độ ẩm từ DHT11
-
-  display.setCursor(0, 40);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.print("Temp: ");
-  display.print(temperature);
-  display.print(" C");
-
-  display.setCursor(0, 50);
-  display.print("Hum: ");
-  display.print(humidity);
-  display.print(" %");
-  if (temperature > NHIET_DO_CAO) {
-    Button_Sound(1); // Bật buzzer
-    delay(10000); // Giữ buzzer kêu trong 10 giây
-    Button_Sound(0); // Tắt buzzer
-  }
+  displayTempHumid();
  
   display.display();
 }
@@ -170,6 +155,27 @@ void Digital_Clock_and_Date(byte h, byte m, byte s, byte dtw, byte dy, byte mt, 
   display.print(_monthsName);
   display.setCursor(95,10);
   display.print(yr);
+}
+void displayTempHumid(){
+  float temperature = dht.readTemperature();  // Đọc giá trị nhiệt độ từ DHT11
+  float humidity = dht.readHumidity();  // Đọc giá trị độ ẩm từ DHT11
+
+  display.setCursor(0, 40);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.print("Temp: ");
+  display.print(temperature);
+  display.print(" C");
+
+  display.setCursor(0, 50);
+  display.print("Hum: ");
+  display.print(humidity);
+  display.print(" %");
+  if (temperature > NHIET_DO_CAO) {
+    Button_Sound(1); // Bật buzzer
+    delay(10000); // Giữ buzzer kêu trong 10 giây
+    Button_Sound(0); // Tắt buzzer
+  }
 }
 void Button_Sound(byte snd) { //điều khiển đầu ra âm thanh của buzzer bằng cách sử dụng hàm digitalWrite(). Tham số snd cho biết trạng thái của buzzer: 1 để bật và 0 để tắt.
   digitalWrite(Buzzer, snd);
